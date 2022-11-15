1410704db84e69ac6f3b068127b2d88f19ec0cf4
iamchrisnov <118233858+iamchrisnov@users.noreply.github.com>
2022-11-15 14:29:48 -0800

    Update testing_audiorecorder.py

--- testing_audiorecorder.py
+++ testing_audiorecorder.py
@@ -3,6 +3,25 @@ import streamlit as st
 from audiorecorder import audiorecorder
 
 model = whisper.load_model("tiny")
+
+def transcribe(audio):
+    #time.sleep(3)
+    # load audio and pad/trim it to fit 30 seconds
+    audio = whisper.load_audio(audio)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions(fp16 = False)
+    result = whisper.decode(model, mel, options)
+    return result.text
+
 print("load complete")
 st.title("Audio Recorder")
 audio = audiorecorder("Click to record", "Recording...")
@@ -17,6 +36,10 @@ if len(audio) > 0:
     wav_file.write(audio.tobytes())
 
     if st.button("Transcribe"):
-        transcription = model.transcribe(audio_filename)
+        transcription = transcribe(audio_filename)
         st.text(transcription["text"])
+
+
+# potentially use gdrive as a storage: https://medium.com/@annissouames99/how-to-upload-files-automatically-to-drive-with-python-ee19bb13dda
+
 
